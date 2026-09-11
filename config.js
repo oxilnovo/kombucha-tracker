ec208ca7f5e1606a3f86c87ffecf32d0e1bbb925
oxilnovo <46940376+oxilnovo@users.noreply.github.com>
2026-09-11 11:23:47 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Paste the deployed Google Apps Script Web App URL here.
+// Example: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
+window.KOMBUCHA_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbwfG_SUj3-cl9EY15st7oBSwVsS0fqPbzIbrGqsAsdF3alI99UZQjWcivLOIUCW3qvD3w/exec"
+};
---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'kombucha-tracker-v1';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './config.js', './manifest.webmanifest', './icon.svg'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.origin !== location.origin) return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
