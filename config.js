077bd4391f0000a5ac92ac67b514278fd1d77263
oxilnovo <46940376+oxilnovo@users.noreply.github.com>
2026-09-11 16:39:08 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Paste the deployed Google Apps Script Web App URL here.
 // Example: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
 window.KOMBUCHA_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxes4VAeqE156SptYXS2KIBC2BjwJdwkgi8j-XtDROiQd8hrArfAQamvKQ-iWrXzQ5xBw/exec"
+  API_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE"
 };
